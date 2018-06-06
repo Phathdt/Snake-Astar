0e6d2a44fc065833464e438bcdbdbefc6c6bf46b
Phathdt <phathdt.leviateam@gmail.com>
2018-06-06 21:48:28 +0700

    Add function fscore and check is inboard

--- node.js
+++ node.js
@@ -11,4 +11,18 @@ class Node {
   heuristicCalcu(to_x, to_y) {
     return Math.floor(Math.abs(to_x - this.x) + Math.abs(to_y - this.y))
   }
+
+  static fScoreSort(FromNode, ToNode) {
+    let dx = FromNode.x - ToNode.x
+    let dy = FromNode.y - ToNode.y
+
+    return Math.hypot(dx, dy)
+  }
+
+  static isInBoard(currNode, i, j) {
+    if (this.board[currNode.y + i][currNode.x + j].block) {
+      return false
+    }
+    return true
+  }
 }
